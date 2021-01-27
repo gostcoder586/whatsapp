5734d83cdf41674ad3d4dfc4737500fe8134f3a3
saichakravarthi585 <nallapanenisaichakravarthi@gmail.com>
2021-01-27 20:34:13 +0530

    mobile number validating feature added

--- Basic file.py
+++ Basic file.py
@@ -1,5 +1,28 @@
-
 import pywhatkit
+import re
 
-pywhatkit.sendwhatmsg("+91 9123456789", "Wish you many more happy returns of the day", 23, 59, 40)
+print(" Note: Make sure you login whatsapp web  ")
+number = input("Enter Mobile Number with out country code: ")
 
+
+def isvalid(number):
+    Pattern = re.compile("(0/91)?[7-9][0-9]{9}")
+    return Pattern.match(number)
+
+
+if (isvalid(number)):
+
+    if len(number) == 10:
+        print("Valid Number Adding country code to the given number")
+        number = "+91" + number
+        message = input("Enter the Message : ")
+        time = input("Enter Time without spaces (HHMMSS)")
+        time_hour = int(time[0:2])
+        time_min = int(time[2:4])
+        time_sec = int(time[4:6])
+        print("Your Message {} will be send to {} at {}".format(message, number, time))
+        pywhatkit.sendwhatmsg(number, message, time_hour, time_min, time_sec)
+    else:
+        print("Invalid Number")
+else:
+    print("Invalid Number")
--- Home.py
+++ Home.py
@@ -1,28 +1,31 @@
-import random
-import threading
-from datetime import datetime
-from time import *
-import keyboard
-
-
-import pywhatkit
-
-msg = random.randint(0, 4)
-messagelist = ['Hi', 'Hello', 'hay', 'hlo', 'are you tare!']
-message = messagelist[msg]
-number1 = ["+91 9553525912", "+91 6302 584 117", "+91 99637 07389"]
-
-
-def sender1():
-    for i in number1:
-        todays_date = datetime.now()
-        print("sender1" + i, todays_date.hour, todays_date.minute + 1)
-        pywhatkit.sendwhatmsg(i, message, todays_date.hour, todays_date.minute + 1)
-        sleep(60)
-        keyboard.press_and_release('ctrl+w')
-
-
-t1 = threading.Thread(target=sender1)
-t1.start()
-t1.join()
-print('All Messages Send successfully')
+# import random
+# import threading
+# import keyboard
+# import pywhatkit
+# from datetime import datetime
+# from time import *
+#
+# msg = random.randint(0, 4)
+# messagelist = ['Hi', 'Hello', 'hay', 'hlo', 'are you tare!']
+# message = messagelist[msg]
+# number = ["+91 9553525912", "+91 6302 584 117", "+91 99637 07389"]
+#
+#
+# def sender1():
+#     for i in number:
+#         todays_date = datetime.now()
+#         print(i, todays_date.hour, todays_date.minute + 1)
+#         pywhatkit.sendwhatmsg(i, message, todays_date.hour, todays_date.minute + 1)
+#         sleep(60)
+#         keyboard.press_and_release('ctrl+w')
+#
+#
+# t1 = threading.Thread(target=sender1)
+# t1.start()
+# t1.join()
+# print('All Messages Send successfully')
+import csv
+with open('contacts.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['Name'], row['Phone 1 - Type'], row['Phone 1 - Value'])
